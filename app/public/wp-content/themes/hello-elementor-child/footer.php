c2e7abb93f2f6a735768ec9dc82e5a05232e78da
Antoine Gaillard <antoine.gaillard59@gmail.com>
2024-01-10 13:30:39 +0100

    maj de planty

---
+++ app/public/wp-content/themes/hello-elementor-child/footer.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the body & html closing tags.
+ *
+ * @package HelloElementor
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'footer' ) ) {
+	if ( hello_elementor_display_header_footer() ) {
+		if ( did_action( 'elementor/loaded' ) && hello_header_footer_experiment_active() ) {
+			get_template_part( 'template-parts/dynamic-footer' );
+		} else {
+			get_template_part( 'template-parts/footer' );
+		}
+	}
+}
+?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
+
+    
+    <style>
+        footer {
+            background-color: #fff; 
+        }
+    </style>
+
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php wp_footer(); ?>
+</body>
+</html>
